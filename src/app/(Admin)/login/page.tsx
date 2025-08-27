2b48023cada31971bb8e735c620df9487ad30761
Dmytrii Tsybuliak <tonistark1997@gmail.com>
2025-08-28 00:15:08 +0300

    login page logic

--- src/app/(Admin)/login/page.tsx
+++ src/app/(Admin)/login/page.tsx
@@ -6,21 +6,37 @@ import { useState } from 'react';
 export default function Login() {
   const signin = useSignin();
 
-  // const [email, setEmail] = useState('');
-  // const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const handleLogin = (e: React.FormEvent) => {
     e.preventDefault();
-
-    signin.mutate({ email: '', password: '2212' });
-    // Handle login logic here
+    signin.mutate({ email: email, password: password });
   };
 
   return (
     <div>
       <form onSubmit={handleLogin} className="flex flex-col gap-4 max-w-sm mx-auto mt-10">
-        <input type="email" placeholder="Email" className="border p-2 rounded" required />
-        <input type="password" placeholder="Password" className="border p-2 rounded" required />
+        <input
+          type="email"
+          placeholder="Email"
+          className="border p-2 rounded"
+          value={email}
+          onChange={e => {
+            setEmail(e.target.value);
+          }}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="border p-2 rounded"
+          value={password}
+          onChange={e => {
+            setPassword(e.target.value);
+          }}
+          required
+        />
         <button type="submit" className="bg-blue-500 text-white p-2 rounded">
           Login
         </button>
