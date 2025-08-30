6f3f766257eb525d24c45c37295f698b3737d75b
Dmytrii Tsybuliak <tonistark1997@gmail.com>
2025-08-31 01:47:24 +0300

    redirect

--- src/lib/react-query.ts
+++ src/lib/react-query.ts
@@ -1,9 +1,17 @@
 import { signin } from '@/lib/api/auth';
 import { useMutation } from '@tanstack/react-query';
+import { redirect } from 'next/navigation';
 
 export function useSignin() {
   return useMutation({
     mutationFn: ({ email, password }: { email: string; password: string }) =>
       signin(email, password),
+    onSuccess: data => {
+      console.log('Login successful:', data);
+      redirect('/dashboard');
+    },
+    onError: error => {
+      console.error('Login failed:', error);
+    },
   });
 }
