33b26b766a4726215f67e4b4214b767d9d359d30
Dmytrii Tsybuliak <tonistark1997@gmail.com>
2025-09-29 14:46:30 +0300

    login form

--- src/app/(Admin)/login/LoginForm.tsx
+++ src/app/(Admin)/login/LoginForm.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import ErrorMessage from '@/components/ErrorMessage';
-import Loader from '@/components/Loader';
+import Loader from '@/components/ui/Loader';
+import Input from '@/components/ui/Input';
 import { useSignin } from '@/lib/hooks/useSignin';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
@@ -26,39 +27,37 @@ export default function LoginForm() {
   return (
     <div className="w-full max-w-xs">
       {signin.isError && <ErrorMessage error={signin.error} />}
+      {signin.isPending && <Loader />}
       <form
         onSubmit={handleLogin}
         className="flex flex-col content-center gap-4 mx-auto mt-10 px-3"
       >
-        <label htmlFor="email">Email</label>
-        <input
+        <Input
           id="email"
           type="email"
-          placeholder="Email"
-          className="border p-3 rounded"
+          label="Email"
+          placeholder="Enter your email"
           value={email}
           onChange={e => {
             setEmail(e.target.value);
           }}
           required
         />
-        <label htmlFor="password">Password</label>
-        <input
+        <Input
           id="password"
           type="password"
-          placeholder="Password"
-          className="border p-3 rounded"
+          label="Password"
+          placeholder="Enter your password"
           value={password}
           onChange={e => {
             setPassword(e.target.value);
           }}
           required
         />
-        {signin.isPending && <Loader />}
         <button
           type="submit"
           disabled={signin.isPending}
-          className="bg-blue-500 text-white p-2 rounded"
+          className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600 transition-colors disabled:opacity-50 disabled:cursor-not-allowed"
         >
           Login
         </button>
