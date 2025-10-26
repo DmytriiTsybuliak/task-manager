cdab42d9e3b346fd6b1e443c7bdad04cc2da3a90
Dmytrii Tsybuliak <tonistark1997@gmail.com>
2025-10-26 19:07:15 +0300

    settings update

--- src/app/(Admin)/dashboard/Navbar.tsx
+++ src/app/(Admin)/dashboard/Navbar.tsx
@@ -12,8 +12,19 @@ export default function NavBar() {
     <div className="flex flex-wrap justify-between items-center p-4 gap-6 bg-gray-800 text-white">
       <h1 className="text-2xl font-bold">Task Dashboard</h1>
       <div className="flex gap-3 items-center ">
-        <SearchBar />
-        <Button onClick={() => setIsModalOpen(true)}>Create</Button>
+        <div className="flex gap-3 items-center">
+          <SearchBar />
+          <Button onClick={() => setIsModalOpen(true)}>Create</Button>
+        </div>
+
+        {/* Filter Completed / Incompleted */}
+        <div>
+          <select className="bg-gray-700 text-white p-2 rounded">
+            <option value="all">All Tasks</option>
+            <option value="completed">Completed Tasks</option>
+            <option value="incomplete">Incomplete Tasks</option>
+          </select>
+        </div>
       </div>
       <Logout />
       <AddTaskModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
